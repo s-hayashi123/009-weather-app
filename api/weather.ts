1b31ef163af7c59c37639eeabb76b74fdc1b7b77
s-hayashi123 <shake7602@gmail.com>
2025-07-22 20:42:43 +0900

    fetch先urlの修正

--- api/weather.ts
+++ api/weather.ts
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { city } = req.query;
   const apikey = process.env.VITE_OPENWEATHER_API_KEY;
-  const apiUrl = `https//api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric&lang=ja`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric&lang=ja`;
   const response = await fetch(apiUrl);
   const data = await response.json();
 
